d0bed74908195e71921ae5f89ce4f6d347a984b8
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-09 16:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function shout(string) {
   return string.toUpperCase()
 }
 function whisper(string) {
-  return string.toLowerCase.toEqual('hello')
+  expect(wisper('Hello')).toEqual('hello')
+  return string.toLowerCase
 }
