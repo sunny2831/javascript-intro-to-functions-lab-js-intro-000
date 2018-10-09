c43ad95475d4065815aa773813e30ba16fce1fe7
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-09 19:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function shout(string) {
   return string.toUpperCase()
-  }
+}
+  
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(string)
 }
+
 function logWhisper(string) {
   console.log(string)
 }
+
 function sayHiToGrandma(string) {
-  return ("I can't hear you!")
+  if `string` is lowercase {
+    return ("I can't hear you")
+  }
+  else if (`string` is uppercase) {
+    return ("YES INDEED!")
+  }
 }
