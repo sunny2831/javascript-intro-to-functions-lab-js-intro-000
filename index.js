6795a0395e96e2fb1246e4f4fc42c6e022d61b50
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-09 19:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return ("I can't hear you!")
   }
-  else if (string.toUppercase() === string) {
-    return ("YES INDEED!")
+  else if (string.toUpperCase() === string) {
+    return ("YES INDEED!")  
   }
 }
